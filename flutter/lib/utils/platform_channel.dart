d1f9608d646a1295f07563b5f4dd467d67a2f559
iW123 <65850431+iW123@users.noreply.github.com>
2026-07-18 22:23:48 +0800

    Update platform_channel.dart

--- flutter/lib/utils/platform_channel.dart
+++ flutter/lib/utils/platform_channel.dart
@@ -9,7 +9,7 @@ enum SystemWindowTheme { light, dark }
 
 /// The platform channel for RustDesk.
 class RdPlatformChannel {
-
+  RdPlatformChannel._();
   static final RdPlatformChannel _windowUtil = RdPlatformChannel._();
 
   static RdPlatformChannel get instance => _windowUtil;
@@ -18,11 +18,11 @@ class RdPlatformChannel {
       MethodChannel("org.rustdesk.rustdesk/host");
 
   RdPlatformChannel._() {
+    File('/tmp/rustdesk_channel_init.log').writeAsStringSync('init\n');
     _hostMethodChannel.setMethodCallHandler((call) async {
       switch (call.method) {
         case "switchHide":
-          File('/tmp/rustdesk_switchHide.log')
-              .writeAsStringSync('received\n');
+          File('/tmp/rustdesk_switchHide.log').writeAsStringSync('received\n');
           for (final state in ToolbarState.states.values) {
             if (state.sessionId != null) {
               state.switchHide(state.sessionId!);
